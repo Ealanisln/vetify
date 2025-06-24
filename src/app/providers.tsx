b1477bc2e1783799aba4b1a8fb57ad14bcd29226
Emmanuel Alanis <ealanisln@me.com>
2025-06-23 17:53:32 -0700

    🎨 Created Appointments page

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,19 @@
 'use client'
 import { ThemeProvider } from 'next-themes'
+import { ReactNode } from 'react'
 
-export function Providers({ children }: { children: React.ReactNode }) {
+interface ProvidersProps {
+  children: ReactNode;
+}
+
+export function Providers({ children }: ProvidersProps) {
   return (
     <ThemeProvider 
       attribute="class"
       defaultTheme="system" 
       enableSystem={true}
       disableTransitionOnChange={false}
+      storageKey="vetify-theme"
     >
       {children}
     </ThemeProvider>
--- src/components/appointments/TodayAppointments.tsx
+++ src/components/appointments/TodayAppointments.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 interface TodayAppointmentsProps {
   tenantId: string;
 }
